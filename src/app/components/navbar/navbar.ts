b42fffbb9f8a47c19f767191bc2fc09b498b2a0c
egarcia9543 <egarcia9543@soy.sena.edu.co>
2026-01-20 17:12:06 -0500

    feat: enhance navbar with routerLinkActive and update pricing page for platform detection

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    RouterLink
-  ],
+    RouterLink,
+    RouterLinkActive
+],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
--- src/app/pages/pricing-page/pricing-page.ts
+++ src/app/pages/pricing-page/pricing-page.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { MetatagsGenerator } from '../../services/metatags-generator';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-pricing-page',
@@ -12,7 +13,21 @@ export class PricingPage {
   private title = inject(Title);
   private _metatagSrv = inject(MetatagsGenerator);
 
+  private _platform = inject(PLATFORM_ID); // Give us info about the platform (server or client)
+
   ngOnInit(): void {
+    /**
+     * We must check if we are in the client or server side
+     */
+
+    console.log('Platform ID:', this._platform);
+
+    // document.title =  "Titulo Hardcoded" - This line works only in the client side, there's no document in the server side
+
+    if (isPlatformBrowser(this._platform)) {
+      console.log('We are in the browser');
+    }
+
     this.title.setTitle(this._metatagSrv.getPageTitle());
   }
 }
