adb3f6b44e0c94a0b50ecccf6d552693b0f5f618
egarcia9543 <egarcia9543@soy.sena.edu.co>
2026-01-25 02:59:10 -0500

    feat: update server routes to use server rendering and remove prerendering configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'characters/page/:page',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      const pages = Array.from({ length: 5 }, (_, i) => ({ page: (i + 1).toString() }));
+      return Promise.resolve(pages);
+    },
+  },
+  {
+    path: 'character/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      const ids = Array.from({ length: 5 }, (_, i) => ({ id: (i + 1).toString() }));
+      return Promise.resolve(ids);
+    }
   }
-];
+]
